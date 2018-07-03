3ddbc2b2b875fbe6c112aac035335c4f0715fcbd
moshokina <moshokina@gmail.com>
2018-07-03 22:45:17 +0300

    move sources in src

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "operations.cpp"
+
+using namespace std;
+int main()
+{
+	cout << "Hello world! I'm Calculator!" << endl;
+	cout << "I can calculate for you" << endl;
+	cout << "1 + 2 = " << add(1,2) << endl;
+}
